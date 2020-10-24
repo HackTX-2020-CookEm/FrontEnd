825976fb5cd9c63548a9f4c82d087c28b8e81602
Eric Wang <ericrwang2011@gmail.com>
2020-10-24 11:59:05 -0500

    feed basic done

--- screens/Home.js
+++ screens/Home.js
@@ -1,60 +1,82 @@
 import React, { Component } from 'react'; 
-import { Image, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Image, StyleSheet, Text, View, FlatList, TextInput, TouchableOpacity } from 'react-native';
 
 const DATA = [
     {
         user_name:'Jasmine Lu',
         user_image:'https://i.pinimg.com/736x/fc/45/6a/fc456aba424730185b1496c75c99c7d2.jpg',
         feed_image:'https://www.chasinglenscapes.com/wp-content/uploads/2020/06/food-photography-on-the-go-tips.jpg',
-        user_comment: 'Check out this amazing dinner I had last night. Absolutely loved it!',
-    }
+        user_comment:'Check out this amazing dinner I had last night. Absolutely loved it!',
+    },
+    {
+        user_name:'Albert Cho',
+        user_image:'https://www.atlanticcouncil.org/wp-content/uploads/2020/06/albert-cho-mlpfellow-2016.jpg',
+        feed_image:'https://miro.medium.com/max/4000/1*yGK9jD35goWdn3XtRq9y4Q.jpeg',
+        user_comment:'Tried to be artsy. Had yummy sushi this afternoon with my buddies. I would definetly recommend this place.',
+    },
+    {
+        user_name:'Jasmine Lu',
+        user_image:'https://i.pinimg.com/736x/fc/45/6a/fc456aba424730185b1496c75c99c7d2.jpg',
+        feed_image:'https://www.chasinglenscapes.com/wp-content/uploads/2020/06/food-photography-on-the-go-tips.jpg',
+        user_comment:'Check out this amazing dinner I had last night. Absolutely loved it!',
+    },
 ]
 
+function Item({ user_name, user_image, feed_image, user_comment }) {
+    return (
+        <View style={styles.card}>
+            <View style={styles.cardHeader}>
+                <View style={styles.headerLeft}>
+                    <Image
+                        style={styles.userImage} 
+                        source={{
+                            uri: user_image,
+                        }}
+                    />
+                    <Text style={styles.userName}>{ user_name }</Text>
+                </View>
+                <View style={styles.headerRight}>
+                </View>
+            </View>
+            <Image
+                style={styles.feedImage} 
+                source={{
+                    uri: feed_image,
+                }}
+            />
+            <View style={styles.cardFooter}>
+                <View style={styles.footerLeft}>
+                    <View style={{flexDirection:'row'}}>
+
+                    </View>
+                </View>
+                <View style={styles.discription}>
+                    <Text>
+                        { user_comment }
+                    </Text>
+                </View>
+            </View>
+        </View>
+    );
+}
+
 export default function Home() {
-    
-    let user_name = DATA[0].user_name;
-    let user_image = DATA[0].user_image;
-    let feed_image = DATA[0].feed_image;
-    let user_comment = DATA[0].user_comment;
 
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.logo}>Home</Text>
             </View>
-            <View style={styles.card}>
-                <View style={styles.cardHeader}>
-                    <View style={styles.headerLeft}>
-                        <Image
-                            style={styles.userImage} 
-                            source={{
-                                uri: user_image,
-                            }}
-                        />
-                        <Text style={styles.userName}>{ user_name }</Text>
-                    </View>
-                    <View style={styles.headerRight}>
-                    </View>
-                </View>
-                <Image
-                    style={styles.feedImage} 
-                    source={{
-                        uri: feed_image,
-                    }}
-                />
-                <View style={styles.cardFooter}>
-                    <View style={styles.footerLeft}>
-                        <View style={{flexDirection:'row'}}>
-
-                        </View>
-                    </View>
-                    <View style={styles.discription}>
-                        <Text>
-                            { user_comment }
-                        </Text>
-                    </View>
-                </View>
-            </View>
+            <FlatList
+                data={DATA}
+                renderItem={({ item }) => <Item title={item.title} 
+                    user_name={item.user_name}
+                    user_image={item.user_image}
+                    feed_image={item.feed_image}
+                    user_comment={item.user_comment}
+                />}
+                keyExtractor={item => item.id}
+            />            
         </View>
     );
 };
